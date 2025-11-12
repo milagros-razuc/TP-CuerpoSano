bbf9d72bb9777a13ef218db84e1dfe9093f6c567
ramacabreraok <ramacabreraok@gmail.com>
2025-11-12 00:05:03 -0300

    Servicios - Fix: Se arreglan los nombres de servicios

--- frontend/src/services/classes.js
+++ frontend/src/services/classes.js
@@ -1,5 +1,5 @@
 import http from './http'
-export const listAll = (params) => http.get('/classes', { params }).then(r=>r.data)
-export const createOne = (payload) => http.post('/classes', payload).then(r=>r.data)
-export const updateOne = (id, payload) => http.put('/classes/'+id, payload).then(r=>r.data)
-export const deleteOne = (id) => http.delete('/classes/'+id).then(r=>r.data)
+export const listAll = (params) => http.get('/clases', { params }).then(r=>r.data)
+export const createOne = (payload) => http.post('/clases', payload).then(r=>r.data)
+export const updateOne = (id, payload) => http.put('/clases/'+id, payload).then(r=>r.data)
+export const deleteOne = (id) => http.delete('/clases/'+id).then(r=>r.data)
--- frontend/src/services/reports.js
+++ frontend/src/services/reports.js
@@ -1,3 +1,3 @@
 import http from './http'
 export const income = (params) => http.get('/reports/income', { params }).then(r=>r.data)
-export const attendance = (params) => http.get('/reports/attendance', { params }).then(r=>r.data)
+export const attendance = (params) => http.get('/asistencias/reportes', { params }).then(r=>r.data)
